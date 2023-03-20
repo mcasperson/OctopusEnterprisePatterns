c11d66941addd4ea33a2bc9bf4c88ef98c7a9312
Matthew Casperson <matthewcasperson@gmail.com>
2023-03-20 12:26:27 +1000

    Added library variable set

---
+++ management_instance/variables/octopus_server/terraform.tf
@@ -0,0 +1,55 @@
+terraform {
+  required_providers {
+    octopusdeploy = { source = "OctopusDeployLabs/octopusdeploy", version = "0.11.0" }
+  }
+}
+
+terraform {
+  backend "s3" {
+  }
+}
+
+provider "octopusdeploy" {
+  address  = "${var.octopus_server}"
+  api_key  = "${var.octopus_apikey}"
+  space_id = "${var.octopus_space_id}"
+}
+
+variable "octopus_server" {
+  type        = string
+  nullable    = false
+  sensitive   = false
+  description = "The URL of the Octopus server e.g. https://myinstance.octopus.app."
+}
+
+variable "octopus_apikey" {
+  type        = string
+  nullable    = false
+  sensitive   = true
+  description = "The API key used to access the Octopus server. See https://octopus.com/docs/octopus-rest-api/how-to-create-an-api-key for details on creating an API key."
+}
+
+variable "octopus_space_id" {
+  type        = string
+  nullable    = false
+  sensitive   = false
+  description = "The ID of the Octopus space to populate."
+}
+
+resource "octopusdeploy_library_variable_set" "octopus_library_variable_set" {
+  name = "Octopus Server"
+  description = "Variables related to interacting with an Octopus server"
+}
+
+resource "octopusdeploy_variable" "octopus_server" {
+  name = "Tenant.Octopus.Server"
+  type = "String"
+  description = "The Octopus Server URL"
+  is_sensitive = false
+  is_editable = true
+  owner_id = octopusdeploy_library_variable_set.octopus_library_variable_set.id
+
+  display_settings {
+    control_type = "SingleLineText"
+  }
+}
