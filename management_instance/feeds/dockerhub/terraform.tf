83390ab226904016a4cb35491ec606bcb2e066a9
Matthew Casperson <matthewcasperson@gmail.com>
2023-03-21 06:34:56 +1000

    Added docker feed credentials

--- management_instance/feeds/dockerhub/terraform.tf
+++ management_instance/feeds/dockerhub/terraform.tf
@@ -36,9 +36,25 @@ variable "octopus_space_id" {
   description = "The ID of the Octopus space to populate."
 }
 
+variable "docker_username" {
+  type        = string
+  nullable    = false
+  sensitive   = false
+  description = "DockerHub username."
+}
+
+variable "docker_password" {
+  type        = string
+  nullable    = false
+  sensitive   = true
+  description = "DockerHub password."
+}
+
 resource "octopusdeploy_docker_container_registry" "feed_docker" {
   name                                 = "Docker"
   api_version                          = "v2"
   feed_uri                             = "https://index.docker.io"
   package_acquisition_location_options = ["ExecutionTarget", "NotAcquired"]
+  username                             = var.docker_username
+  password                             = var.docker_password
 }
