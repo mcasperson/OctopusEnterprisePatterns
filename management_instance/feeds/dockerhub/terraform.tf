230ee5105876358f563ab875f7f1b20d89d0272a
Matthew Casperson <matthewcasperson@gmail.com>
2023-03-20 14:32:03 +1000

    Add the docker hub feed

---
+++ management_instance/feeds/dockerhub/terraform.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_providers {
+    octopusdeploy = { source = "OctopusDeployLabs/octopusdeploy", version = "0.11.0" }
+  }
+}
+
+terraform {
+  backend "s3" {
+  }
+}
+
+provider "octopusdeploy" {
+  address  = "${var.octopus_server}"
+  api_key  = "${var.octopus_apikey}"
+  space_id = "${var.octopus_space_id}"
+}
+
+variable "octopus_server" {
+  type        = string
+  nullable    = false
+  sensitive   = false
+  description = "The URL of the Octopus server e.g. https://myinstance.octopus.app."
+}
+
+variable "octopus_apikey" {
+  type        = string
+  nullable    = false
+  sensitive   = true
+  description = "The API key used to access the Octopus server. See https://octopus.com/docs/octopus-rest-api/how-to-create-an-api-key for details on creating an API key."
+}
+
+variable "octopus_space_id" {
+  type        = string
+  nullable    = false
+  sensitive   = false
+  description = "The ID of the Octopus space to populate."
+}
+
+resource "octopusdeploy_docker_container_registry" "feed_docker" {
+  name                                 = "Docker"
+  api_version                          = "v2"
+  feed_uri                             = "https://index.docker.io"
+  package_acquisition_location_options = ["ExecutionTarget", "NotAcquired"]
+}
