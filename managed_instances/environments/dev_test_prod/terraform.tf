5903ffd8a8d8053d6f1d0c9c3c5db50f4b6e8e1a
Matthew Casperson <matthewcasperson@gmail.com>
2023-03-21 16:20:25 +1000

    Added a security lifecycle

--- managed_instances/environments/dev_test_prod/terraform.tf
+++ managed_instances/environments/dev_test_prod/terraform.tf
@@ -95,3 +95,112 @@ resource "octopusdeploy_environment" "environment_production" {
     is_enabled = false
   }
 }
+
+resource "octopusdeploy_environment" "environment_security" {
+  name                         = "Security"
+  description                  = ""
+  allow_dynamic_infrastructure = true
+  use_guided_failure           = false
+  sort_order                   = 3
+
+  jira_extension_settings {
+    environment_type = "unmapped"
+  }
+
+  jira_service_management_extension_settings {
+    is_enabled = false
+  }
+
+  servicenow_extension_settings {
+    is_enabled = false
+  }
+}
+
+resource "octopusdeploy_lifecycle" "devsecops_lifecycle" {
+  description = "Lifecycle including security scanning"
+  name        = "DevSecOps"
+
+  release_retention_policy {
+    quantity_to_keep    = 1
+    should_keep_forever = true
+    unit                = "Days"
+  }
+
+  tentacle_retention_policy {
+    quantity_to_keep    = 30
+    should_keep_forever = false
+    unit                = "Items"
+  }
+
+  phase {
+    automatic_deployment_targets = []
+    optional_deployment_targets  = [octopusdeploy_environment.environment_dev.id]
+    name                         = octopusdeploy_environment.environment_dev.name
+
+    release_retention_policy {
+      quantity_to_keep    = 1
+      should_keep_forever = true
+      unit                = "Days"
+    }
+
+    tentacle_retention_policy {
+      quantity_to_keep    = 30
+      should_keep_forever = false
+      unit                = "Items"
+    }
+  }
+
+  phase {
+    automatic_deployment_targets = []
+    optional_deployment_targets  = [octopusdeploy_environment.environment_test.id]
+    name                         = octopusdeploy_environment.environment_test.name
+
+    release_retention_policy {
+      quantity_to_keep    = 1
+      should_keep_forever = true
+      unit                = "Days"
+    }
+
+    tentacle_retention_policy {
+      quantity_to_keep    = 30
+      should_keep_forever = false
+      unit                = "Items"
+    }
+  }
+
+  phase {
+    automatic_deployment_targets = []
+    optional_deployment_targets  = [octopusdeploy_environment.environment_production.id]
+    name                         = octopusdeploy_environment.environment_production.name
+
+    release_retention_policy {
+      quantity_to_keep    = 30
+      should_keep_forever = true
+      unit                = "Days"
+    }
+
+    tentacle_retention_policy {
+      quantity_to_keep    = 30
+      should_keep_forever = false
+      unit                = "Items"
+    }
+  }
+
+  phase {
+    automatic_deployment_targets = [octopusdeploy_environment.environment_security.id]
+    optional_deployment_targets  = []
+    name                         = octopusdeploy_environment.environment_security.name
+
+    release_retention_policy {
+      quantity_to_keep    = 30
+      should_keep_forever = true
+      unit                = "Days"
+    }
+
+    tentacle_retention_policy {
+      quantity_to_keep    = 30
+      should_keep_forever = false
+      unit                = "Items"
+    }
+  }
+}
