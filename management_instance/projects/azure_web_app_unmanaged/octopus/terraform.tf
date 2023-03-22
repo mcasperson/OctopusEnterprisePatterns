64d5c0131650ef88fbc85986f1f899f0020e287a
Matthew Casperson <matthewcasperson@gmail.com>
2023-03-23 09:36:31 +1000

    Fixed variable quotes

--- management_instance/projects/azure_web_app_unmanaged/octopus/terraform.tf
+++ management_instance/projects/azure_web_app_unmanaged/octopus/terraform.tf
@@ -155,7 +155,7 @@ resource "octopusdeploy_deployment_process" "deployment_process" {
         "Octopus.Action.Terraform.GoogleCloudAccount" = "False"
         "Octopus.Action.Package.DownloadOnTentacle" = "False"
         "Octopus.Action.Terraform.AdditionalInitParams" = ""
-        "Octopus.Action.Terraform.AdditionalActionParams" = "-var=octopus_server=#{Tenant.Octopus.Server} -var=octopus_apikey=#{Tenant.Octopus.ApiKey} -var=octopus_space_id=#{Tenant.Octopus.SpaceId} \"-var=existing_project_group=Default Project Group\" -var=project_name=#{Project.Name}"
+        "Octopus.Action.Terraform.AdditionalActionParams" = "-var=octopus_server=#{Tenant.Octopus.Server} -var=octopus_apikey=#{Tenant.Octopus.ApiKey} -var=octopus_space_id=#{Tenant.Octopus.SpaceId} \"-var=existing_project_group=Default Project Group\" \"-var=project_name=#{Project.Name}\""
         "Octopus.Action.Terraform.Workspace" = "#{Octopus.Deployment.Tenant.Name | ToLower}"
       }
       environments                       = []
