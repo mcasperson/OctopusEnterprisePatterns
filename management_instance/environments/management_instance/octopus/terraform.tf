2b9f555b9e2652b30459c06ce91c709b0f690787
Matthew Casperson <matthewcasperson@gmail.com>
2023-03-30 07:44:49 +1000

    Add the template project

---
+++ management_instance/environments/management_instance/octopus/terraform.tf
@@ -0,0 +1,60 @@
+terraform {
+  required_providers {
+    octopusdeploy = { source = "OctopusDeployLabs/octopusdeploy", version = "0.11.1" }
+  }
+}
+
+resource "octopusdeploy_environment" "environment_managed_instances" {
+  name                         = "Managed Instances"
+  description                  = ""
+  allow_dynamic_infrastructure = true
+  use_guided_failure           = false
+  sort_order                   = 2
+
+  jira_extension_settings {
+    environment_type = "unmapped"
+  }
+
+  jira_service_management_extension_settings {
+    is_enabled = false
+  }
+
+  servicenow_extension_settings {
+    is_enabled = false
+  }
+}
+
+resource "octopusdeploy_lifecycle" "managed_instance_lifecycle" {
+  description = "Lifecycle used when managing octopus spaces and instances"
+  name        = "Managed Instance"
+
+  release_retention_policy {
+    quantity_to_keep    = 1
+    should_keep_forever = true
+    unit                = "Days"
+  }
+
+  tentacle_retention_policy {
+    quantity_to_keep    = 30
+    should_keep_forever = false
+    unit                = "Items"
+  }
+
+  phase {
+    automatic_deployment_targets = []
+    optional_deployment_targets  = [octopusdeploy_environment.environment_managed_instances.id]
+    name                         = octopusdeploy_environment.environment_managed_instances.name
+
+    release_retention_policy {
+      quantity_to_keep    = 1
+      should_keep_forever = true
+      unit                = "Days"
+    }
+
+    tentacle_retention_policy {
+      quantity_to_keep    = 30
+      should_keep_forever = false
+      unit                = "Items"
+    }
+  }
+}
