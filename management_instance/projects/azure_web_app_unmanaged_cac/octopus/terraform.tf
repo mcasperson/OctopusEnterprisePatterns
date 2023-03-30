b808cc375b3ef66d45feac23813133f7ea8596b0
Matthew Casperson <matthewcasperson@gmail.com>
2023-03-30 14:37:57 +1000

    Fixed up the repo path

--- management_instance/projects/azure_web_app_unmanaged_cac/octopus/terraform.tf
+++ management_instance/projects/azure_web_app_unmanaged_cac/octopus/terraform.tf
@@ -211,7 +211,7 @@ resource "octopusdeploy_deployment_process" "deployment_process" {
         "Octopus.Action.Terraform.GoogleCloudAccount" = "False"
         "Octopus.Action.Package.DownloadOnTentacle" = "False"
         "Octopus.Action.Terraform.AdditionalInitParams" = ""
-        "Octopus.Action.Terraform.AdditionalActionParams" = "-var=octopus_server=#{Tenant.Octopus.Server} -var=octopus_apikey=#{Tenant.Octopus.ApiKey} -var=octopus_space_id=#{Tenant.Octopus.SpaceId} -var=cac_url=#{Octopus.Deployment.Tenant.Name | ToLower}-#{Project.Name | ToLower | Replace \"[^a-zA-Z0-9]\" \"-\"} \"-var=existing_project_group=Default Project Group\" \"-var=project_name=#{Project.Name}\""
+        "Octopus.Action.Terraform.AdditionalActionParams" = "-var=octopus_server=#{Tenant.Octopus.Server} -var=octopus_apikey=#{Tenant.Octopus.ApiKey} -var=octopus_space_id=#{Tenant.Octopus.SpaceId} -var=cac_url=#{Tenant.CaC.Url}/#{Octopus.Deployment.Tenant.Name | ToLower}-#{Project.Name | ToLower | Replace \"[^a-zA-Z0-9]\" \"-\"}.git \"-var=existing_project_group=Default Project Group\" \"-var=project_name=#{Project.Name}\""
         "Octopus.Action.Terraform.Workspace" = "#{Octopus.Deployment.Tenant.Name | ToLower}"
       }
       environments                       = []
